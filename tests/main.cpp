f7a9e39158d1586d9e76bef8d0d630246b8e8271
Hüseyin Karakaya <karakayahuseyin023@gmail.com>
2025-03-23 02:16:35 +0300

    feat: add doctest testing

---
+++ tests/main.cpp
@@ -0,0 +1,13 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest/doctest.h"
+#include <Airframe/Airframe.hpp>
+
+/**
+ * Doctest implementation tests.
+ */
+
+TEST_CASE("Airframe test")
+{
+    Airframe airframe;
+    CHECK(true);
+}
